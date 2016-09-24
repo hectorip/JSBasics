857ea0b0e8874c99d0c84f7c77c9628e45606c54
Hector Ivan Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-09-23 23:53:48 -0500

    WIP

--- js/main.js
+++ js/main.js
@@ -3,13 +3,19 @@ console.log("mensaje");
 // alert("Esto es un Alert");
 // prompt("Dame un número: ");
 var edad = prompt("Dime tu edad: ");
+var conteo = 0;
 // console.log("La edad del usuario es: " + edad);
 
 if(edad >= 18){
     alert("El usuario es mayor de edad");
 } else {
     alert("El usuario no es mayor de edad");
+    conteo++;
 }
+
+
+
+
 
 
 
