2b1115f09976ef8a401cdd2766db36d9ef100623
Hector Ivan Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-09-26 23:43:35 -0500

    Objetos y arrays

---
+++ js/ciclos.js
@@ -0,0 +1,39 @@
+// Colecciones: arrays
+var edades = [10, 20, 30];
+var numeros = ["five", 5, "go", false];
+
+// Colecciones: diccionarios // objeto
+
+var colores = {
+    "rojo": "#F00",
+    "azul": "#"
+};
+
+// obejto
+var persona = {
+    // propiedades - cosas que es o tiene - 9
+    nombre: "Héctor",
+    apellido: "Patricio",
+    edad: 27,
+    sexo: "H",
+    hobbies: ["programar", "leer"],
+    // métodos - cosas que hace (comportamiento) - 3
+    saludar: function(nombre){
+        alert("Hola " +  nombre);
+    },
+    presentar: function(){
+        alert("Hola me llamo " + nombre);
+    },
+};
+
+
+// ciclos - loops
+
+
+
+
+
+
+
+
+
